dccfa39b7d87e66f910f9628d4000fdab113931d
IsaevEA <erjanisaev42@gmail.com>
2024-10-13 19:45:22 +0600

    Transaction logic changed

--- src/main/java/org/example/kassa/Enum/StatusTransaction.java
+++ src/main/java/org/example/kassa/Enum/StatusTransaction.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 @Getter
 public enum StatusTransaction {
     PAYMENT_SUCCESSFUL(250, "Платеж успешно выдана"),
-    TRANSACTION_NOT_FOUNT(400, "Платеж нет найден"),
-    TRANSACTION_CLOSED(421, "Платеж закрыт");
+    TRANSACTION_NOT_FOUNT(400, "Платеж не найден"),
+    TRANSACTION_CLOSED(421, "Платеж закрыт"),
+    TRANSACTION_SUCCESSFUL(200, "Платеж успешно отправлен");
+
 
     private final int code;
     private final String message;
